24727c8c0c5a47c9d749808061f3af75ea75f9d7
faithOta1 <faith_ota1@baylor.edu>
2025-03-24 17:55:55 -0500

    Created a User class for TYPES of users to extend from

---
+++ PawPlates/src/main/java/org/example/User.java
@@ -0,0 +1,21 @@
+package org.example;
+
+/*
+ * PURPOSE: provide a basic structure for ALL users (all users need a login)
+ */
+public abstract class User {
+    String username;
+    String password;
+
+    final void login(String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    final void setUsername(String username){
+        this.username = username;
+    }
+    final void setPassword(String password){
+        this.password = password;
+    }
+}
