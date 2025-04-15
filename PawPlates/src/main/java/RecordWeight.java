d8659efcc6afbf0120b259152aeb8c757bdfb08c
Eli Hall <Eli_Hall2@baylor.edu>
2025-04-15 12:12:37 -0500

    New Record Weight class

---
+++ PawPlates/src/main/java/RecordWeight.java
@@ -0,0 +1,56 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class RecordWeight extends TemplateFrame{
+    private static final int GOAL_WEIGHT = 145;
+    private static final int START_WEIGHT = 165;
+    private static int curWeight = START_WEIGHT;
+    private static JProgressBar weightProgress;
+    private static JLabel progressLabel;
+
+    public RecordWeight() {
+        weightProgress = new JProgressBar(0, GOAL_WEIGHT);
+        progressLabel = new JLabel(getProgressText());
+
+        JPanel progressPanel = new JPanel(new BorderLayout());
+        progressPanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        progressPanel.add(progressLabel, BorderLayout.NORTH);
+        progressPanel.add(weightProgress, BorderLayout.CENTER);
+
+        JTextField newWeight = new JTextField(15);
+        addTextField("Current Weight:", newWeight, 0);
+
+        JButton confirm = new JButton("Confirm");
+        addButton(confirm, 5);
+
+        confirm.addActionListener(e -> {
+            String inputWeight = newWeight.getText();
+            int weight = Integer.parseInt(inputWeight);
+
+            if(weight > GOAL_WEIGHT && weight < curWeight){
+                JOptionPane.showMessageDialog(this,
+                        "Great work, you're making progress towards your weight goal");
+            } else if(weight <= GOAL_WEIGHT){
+                JOptionPane.showMessageDialog(this,
+                        "Congrats, you've met your weight goal!");
+            } else {
+                JOptionPane.showMessageDialog(this,
+                        "Uh oh, double check your plan to stay on track");
+            }
+
+            curWeight = weight;
+            weightProgress.setValue(Math.min(curWeight, GOAL_WEIGHT));
+            progressLabel.setText(getProgressText());
+        });
+
+        contentPane.add(contentPanel, BorderLayout.CENTER);
+        contentPane.add(progressPanel, BorderLayout.NORTH);
+        setVisible(true);
+    }
+
+    private static String getProgressText() {
+        return String.format("Weight Goal Progress: %d → %d lbs", curWeight, GOAL_WEIGHT);
+    }
+
+    public static void main(String[] args) { new RecordWeight(); }
+}
